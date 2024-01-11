91fae7751d516c579a2da9fa8ec8534d60c053d2
Elise Ngo <elisetvy@gmail.com>
2024-01-11 12:37:59 -0800

    Improve speed w/ sliding window

--- maximumAverageSubarrayI.js
+++ maximumAverageSubarrayI.js
@@ -1,18 +1,19 @@
 // https://leetcode.com/problems/maximum-average-subarray-i/?envType=study-plan-v2&envId=leetcode-75
 
 var findMaxAverage = function(nums, k) {
-  let max = -Infinity;
+  let currSum = nums.slice(0, k).reduce((a, b) => a + b);
+  let maxAvg = nums.slice(0, k).reduce((a, b) => a + b) / k;
 
-  for (let i = 0; i <= nums.length - k; i++) {
-    let currAverage = nums.slice(i, i + k).reduce((a, b) => a + b) / k;
+  for (let i = 1; i <= nums.length - k; i++) {
+    currSum = currSum - nums[i - 1] + nums[i + k - 1];
 
-    if (currAverage > max) {
-      max = currAverage
+    if (currSum / k > maxAvg) {
+      maxAvg = currSum / k;
     }
   }
 
-  return max;
+  return maxAvg;
 };
 
 console.log(findMaxAverage([1,12,-5,-6,50,3], 4));
-console.log(findMaxAverage([5], 1))
+console.log(findMaxAverage([5], 1));
